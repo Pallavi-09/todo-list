a82f9b5c921b50ed970803bf399b49945d114d4c
Pallavi Singh <pallavisingh992@gmail.com>
2018-01-09 17:12:27 +0530

    working on todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pallavi';
+  title = 'ToDo List App';
+
+  constructor() {
+      this.closeElmt = "\u00D7";
+  }
+
+  closeElmt : string;
+
+  lang : any[] = [
+    {name : 'Angular2'},
+    {name : 'React'},
+    {name : 'React Native'},
+    {name : 'Javascript'},
+    {name : 'HTML5'},
+    {name : 'CSS'},    
+  ];
+
+  addList(newList){
+    this.lang.push(newList);
+    console.log(this.lang);
+  }
+
+  deleteData(event){
+    let deleteTxt = event.currentTarget;
+    let deleteParent = deleteTxt.parentElement;
+    deleteParent.remove();
+  }
+}
